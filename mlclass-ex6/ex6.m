3be42db6d727680f551444412cbe6e2f3330f8eb
Roman Kamyk <roman.kamyk@gmail.com>
2011-11-27 17:14:41 -0800

    task2 - parameters

--- mlclass-ex6/ex6.m
+++ mlclass-ex6/ex6.m
@@ -63,8 +63,6 @@ sim = gaussianKernel(x1, x2, sigma);
 fprintf(['Gaussian Kernel between x1 = [1; 2; 1], x2 = [0; 4; -1], sigma = 0.5 :' ...
          '\n\t%f\n(this value should be about 0.324652)\n'], sim);
 
-return
-
 %% =============== Part 4: Visualizing Dataset 2 ================
 %  The following code will load the next dataset into your environment and 
 %  plot the data. 
@@ -77,10 +75,7 @@ fprintf('Loading and Visualizing Data ...\n')
 load('ex6data2.mat');
 
 % Plot training data
-plotData(X, y);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%plotData(X, y);
 
 %% ========== Part 5: Training SVM with RBF Kernel (Dataset 2) ==========
 %  After you have implemented the kernel, we can now use it to train the 
@@ -98,11 +93,8 @@ C = 1; sigma = 0.1;
 % We set the tolerance and max_passes lower here so that the code will run
 % faster. However, in practice, you will want to run the training to
 % convergence.
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
-visualizeBoundary(X, y, model);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+%visualizeBoundary(X, y, model);
 
 %% =============== Part 6: Visualizing Dataset 3 ================
 %  The following code will load the next dataset into your environment and 
@@ -118,9 +110,6 @@ load('ex6data3.mat');
 % Plot training data
 plotData(X, y);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
 %% ========== Part 7: Training SVM with RBF Kernel (Dataset 3) ==========
 
 %  This is a different dataset that you can use to experiment with. Try
@@ -134,10 +123,19 @@ load('ex6data3.mat');
 % Try different SVM Parameters here
 [C, sigma] = dataset3Params(X, y, Xval, yval);
 
+bestC = C; bestSigma = sigma; bestError = 100000;
 % Train the SVM
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-visualizeBoundary(X, y, model);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%for C = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+  %for sigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    pred = svmPredict(model, Xval);
+    error = mean(double(pred ~= yval));
+    if error < bestError
+      bestC = C; bestSigma = sigma; bestError = error;
+    end
+    [C, sigma, error]
+    visualizeBoundary(X, y, model);
+  %end
+%end
+[bestC, bestSigma, bestError]
 
